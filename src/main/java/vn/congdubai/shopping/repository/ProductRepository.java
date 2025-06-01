366088ea9cff852050adeeccee7420c878c532fa
congdubai <qnc2755@gmail.com>
2025-06-01 18:19:39 +0700

    mupdate query search

--- src/main/java/vn/congdubai/shopping/repository/ProductRepository.java
+++ src/main/java/vn/congdubai/shopping/repository/ProductRepository.java
@@ -17,44 +17,45 @@ import vn.congdubai.shopping.domain.response.ProductDTO;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
-  @Modifying
-  @Transactional
-  @Query("UPDATE Product p SET p.isDeleted = true WHERE p.id = :id")
-  void softDeleteProduct(@Param("id") Long id);
+    @Modifying
+    @Transactional
+    @Query("UPDATE Product p SET p.isDeleted = true WHERE p.id = :id")
+    void softDeleteProduct(@Param("id") Long id);
 
-  boolean existsByName(String name);
+    boolean existsByName(String name);
 
-  @Query("SELECT p FROM Product p WHERE p.isDeleted = false AND p.category.gender = :gender")
-  Page<Product> findByCategoryGender(@Param("gender") vn.congdubai.shopping.util.constant.GenderEnum gender,
-      Pageable pageable);
+    @Query("SELECT p FROM Product p WHERE p.isDeleted = false AND p.category.gender = :gender")
+    Page<Product> findByCategoryGender(@Param("gender") vn.congdubai.shopping.util.constant.GenderEnum gender,
+            Pageable pageable);
 
-  List<Product> findByNameContainingIgnoreCase(String name);
+    List<Product> findByNameContainingIgnoreCase(String name);
 
-  @Query("""
-          SELECT new vn.congdubai.shopping.domain.response.ProductDTO(
-              p.id,
-              p.name,
-              p.price,
-              c.name,
-              AVG(r.rating),
-              p.image
-          )
-          FROM Product p
-          LEFT JOIN p.productDetails pd
-          LEFT JOIN pd.color c
-          LEFT JOIN p.reviews r
-          WHERE (:categoryId IS NULL OR p.category.id = :categoryId)
-            AND (p.price BETWEEN :minPrice AND :maxPrice)
-            AND (:colorId IS NULL OR c.id = :colorId)
-            AND (LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%')))
-          GROUP BY p.id, p.name, p.price, c.name, p.image
-      """)
-  List<ProductDTO> searchProducts(
-      @Param("categoryId") Long categoryId,
-      @Param("minPrice") Double minPrice,
-      @Param("maxPrice") Double maxPrice,
-      @Param("colorId") Long colorId,
-      @Param("rating") Long rating,
-      @Param("name") String name);
+    @Query("""
+                SELECT new vn.congdubai.shopping.domain.response.ProductDTO(
+                    p.id,
+                    p.name,
+                    p.price,
+                    MAX(c.name),
+                    COALESCE(AVG(r.rating), 0.0),
+                    p.image
+                )
+                FROM Product p
+                LEFT JOIN p.productDetails pd
+                LEFT JOIN pd.color c
+                LEFT JOIN p.reviews r
+                WHERE (:categoryId IS NULL OR p.category.id = :categoryId)
+                  AND (p.price BETWEEN :minPrice AND :maxPrice)
+                  AND (:colorId IS NULL OR c.id = :colorId)
+                  AND (LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%')))
+                GROUP BY p.id, p.name, p.price, p.image
+                HAVING (:rating IS NULL OR COALESCE(AVG(r.rating), 0.0) >= :rating)
+            """)
+    List<ProductDTO> searchProducts(
+            @Param("categoryId") Long categoryId,
+            @Param("minPrice") Double minPrice,
+            @Param("maxPrice") Double maxPrice,
+            @Param("colorId") Long colorId,
+            @Param("rating") Long rating,
+            @Param("name") String name);
 
 }
